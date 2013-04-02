10d25bfe8aecbaa1422feaad27ff80a8b010b933
Caleb Reed <reedcaleb@gmail.com>
2013-04-02 01:04:36 -0400

    Added modal and formatted about us and other things

--- app/models/prof.rb
+++ app/models/prof.rb
@@ -1,5 +1,11 @@
 class Prof < ActiveRecord::Base
   attr_accessible :con, :email, :name, :pro
+
+validates :name, presence: true
+validates :email, presence: true
+validates :pro, presence: true
+validates :con, presence: true
+
 belongs_to :user
 has_attached_file :image
 end
