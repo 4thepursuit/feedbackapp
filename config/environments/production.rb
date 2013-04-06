a0dca39d7dd79010b14adf32fc4f7904f4d4dd3a
Caleb Reed <reedcaleb@gmail.com>
2013-04-06 14:29:08 -0400

    work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Feedbackapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-
+  GA.tracker = "UA-39906688-1"
+  
   # Code is not reloaded between requests
   config.cache_classes = true
 
