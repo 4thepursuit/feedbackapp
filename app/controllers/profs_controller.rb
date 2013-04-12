c0da243adf1a52f09e929273d440993e763dd11e
Caleb Reed <reedcaleb@gmail.com>
2013-04-12 03:13:17 -0400

    actual fix

--- app/controllers/profs_controller.rb
+++ app/controllers/profs_controller.rb
@@ -1,5 +1,5 @@
 class ProfsController < ApplicationController
-before_filter :authenticate_user!, except: [:new, :create]
+before_filter :authenticate_user!, except: [:new, :create, :show, :edit]
 
 
   # GET /profs
